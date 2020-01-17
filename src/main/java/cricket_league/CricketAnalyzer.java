b36468edee8145c2e1ecabbaea0d958dc3e1ed50
AmarArjunGunjal <amargunjal28@gmail.com>
2020-01-17 14:58:12 +0530

    Added : Method getTopBattingAverage and test case to sort according to average

--- src/main/java/cricket_league/CricketAnalyzer.java
+++ src/main/java/cricket_league/CricketAnalyzer.java
@@ -8,15 +8,19 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CricketAnalyzer {
+
+    List<IPLBattingCsv> iplBattingCsvList = new ArrayList<>();
 
     public int loadBattingDataFile(String csvFilePath) {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            List<IPLBattingCsv> csvBattingDataList = csvBuilder.getCSVFileList(reader,IPLBattingCsv.class);
-            return csvBattingDataList.size();
+            iplBattingCsvList = csvBuilder.getCSVFileList(reader,IPLBattingCsv.class);
+            return iplBattingCsvList.size();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (CsvBuilderException e) {
@@ -25,4 +29,11 @@ public class CricketAnalyzer {
         return 0;
     }
 
+    public List getTopBattingAverages() {
+        iplBattingCsvList = iplBattingCsvList.stream()
+                .sorted((data1,data2) -> data1.avg - data2.avg > 0 ? -1 : 1)
+                .collect(Collectors.toList());
+        return iplBattingCsvList;
+
+    }
 }
--- src/test/java/cricket_league/Batting_Test.java
+++ src/test/java/cricket_league/Batting_Test.java
@@ -3,6 +3,8 @@ package cricket_league;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 
 public class Batting_Test {
 
@@ -12,6 +14,17 @@ public class Batting_Test {
     public void givenBattingCsvFile_shouldReturnCsvFileData() {
         CricketAnalyzer cricketAnalyzer = new CricketAnalyzer();
         int noOfRecords = cricketAnalyzer.loadBattingDataFile(IPL_2019_BATTING_FILE_PATH);
+        System.out.println(noOfRecords);
         Assert.assertEquals(101,noOfRecords);
+    }
+
+    @Test
+    public void givenBattingCsvFile_shouldReturnTopBattingAverages() {
+        CricketAnalyzer cricketAnalyzer = new CricketAnalyzer();
+        cricketAnalyzer.loadBattingDataFile(IPL_2019_BATTING_FILE_PATH);
+        List<IPLBattingCsv>list = cricketAnalyzer.getTopBattingAverages();
+        Assert.assertTrue(list.get(0).toString().contains("MS Dhoni"));
+        Assert.assertEquals(83.2,list.get(0).avg,0);
+        Assert.assertEquals(0.0,list.get(100).avg,0);
     }
 }
